455d3b6f36ecd3776c7b04199dd9f8c9332749c5
aleeyus <scamingsd@gmail.com>
2018-10-15 16:52:29 +0100

    Line #5 switch to #6

--- default.js
+++ default.js
@@ -2,5 +2,7 @@
 
 document.writeln("This is my first javascript github commit")
 console.log("Github is sexy")
+document.writeln("")
 console.error("NULL")
 console.warn("This program may contain deadlock!")
+document.write("Sucess")
